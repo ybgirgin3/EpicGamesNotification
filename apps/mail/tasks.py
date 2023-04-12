d731881633967db47956f5066c1710b51b50a696
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-04-12 23:40:13 +0300

    reading from csv and creating html table for email is done but messy :(

--- apps/mail/tasks.py
+++ apps/mail/tasks.py
@@ -1,16 +1,17 @@
 import logging
 import smtplib
-from tabulate import tabulate
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from typing import Optional
+
+from tabulate import tabulate
 
 
 class Mail:
     sender = "epicfreegamesreminder@outlook.com"
     subject = "Free Epic Games Reminder"
     message = "You weekly Epic Games Reminder Is Here;"
-    email = MIMEMultipart("alternative")
+    # email = MIMEMultipart("alternative")
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
 
     logging.debug(f'sender: {sender}, subject: {subject}, message: {message}')
@@ -19,10 +20,11 @@ class Mail:
         self.recipient: str = "ybgirgin3@gmail.com"  # senders will be list reading by db
 
     def send(self):
+        self.email = self.create_html_body()
         self.email["From"] = self.sender
         self.email["To"] = self.recipient
         self.email["Subject"] = self.subject
-        self.email.attach(self.create_html_body())
+        # self.email.attach(self.create_html_body())
 
         self.smtp.starttls()
         self.smtp.login(self.sender, "/P_VK5sH!SCi4#f")
@@ -30,21 +32,23 @@ class Mail:
         self.smtp.quit()
 
     def create_html_body(self):
-        # self.email.attach(MIMEText("""\
-        body = f"""
+        body = """
             <html>
               <body>
                 <p>Hi,<br>
                    How are you?<br>
-                   {self.message}
+                   "You weekly Epic Games Reminder Is Here
                    <a href="http://ybgirgin3.github.io">Bekocan personal site</a> 
-                {self.table}
+                {table}
                 </p>
               </body>
             </html>
             """
         logging.debug('body of the mail {}'.format(body))
-        return MIMEText(body, "html")
+        import csv
+        with open('../freegames.csv') as inpf:
+            data = list(csv.reader(inpf))
+        html = body.format(table=tabulate(data, headers="firstrow", tablefmt="html"))
 
-
-
+        return MIMEMultipart(
+            "alternative", None, [MIMEText(html, 'html')])
