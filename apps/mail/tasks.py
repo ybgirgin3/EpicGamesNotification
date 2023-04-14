a673420df68c1c4f944f3881c3a409d35f24a8d7
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-04-15 01:58:27 +0300

    new order of temp mail template

--- apps/mail/tasks.py
+++ apps/mail/tasks.py
@@ -16,9 +16,9 @@ class Mail:
 
     logging.debug(f'sender: {sender}, subject: {subject}, message: {message}')
 
-
     def __init__(self, rec: Optional[list] = None, data: pd.DataFrame = "") -> None:
-        self.recipient: str = "ybgirgin3@gmail.com"  # senders will be list reading by db
+        # senders will be list reading by db
+        self.recipient: str = "ybgirgin3@gmail.com"
         self.data: str = data
 
     def send(self):
@@ -30,7 +30,8 @@ class Mail:
 
         self.smtp.starttls()
         self.smtp.login(self.sender[0], self.sender[1])
-        self.smtp.sendmail(self.sender[0], self.recipient, self.email.as_string())
+        self.smtp.sendmail(
+            self.sender[0], self.recipient, self.email.as_string())
         self.smtp.quit()
 
     def create_html_body(self):
@@ -40,13 +41,16 @@ class Mail:
                 <p>Hi,<br>
                    How are you?<br>
                    You weekly Epic Games Reminder Is Here
-                   <a href="http://ybgirgin3.github.io">Bekocan personal site</a> 
                 {self.data.to_html()}
                 <br>
-                Find an issue? Let us know
-                <a href='https://github.com/ybgirgin3/EpicGamesNotification'>https://github.com/ybgirgin3/EpicGamesNotification</a>
-                </p>
+               </p>
               </body>
+              <footer>
+                Find an issue? Let me know
+                <a href='https://github.com/ybgirgin3/EpicGamesNotification'>https://github.com/ybgirgin3/EpicGamesNotification</a>
+                <br>
+                Contact me <a href="http://ybgirgin3.github.io">Yusuf Berkay Girgin Personal Website</a> 
+              </footer>
             </html>
             """
         logging.debug('body of the mail {}'.format(body))
