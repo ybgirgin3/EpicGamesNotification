05a0c8d27867d0841dcad8357ae58a55a4b27eea
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-12-03 20:12:59 +0300

    new link

--- epicgamesnotification/apps/scrape/steampowered.py
+++ epicgamesnotification/apps/scrape/steampowered.py
@@ -2,9 +2,9 @@ import requests
 
 
 class SteamPowered:
-    base_url = 'https://store.steampowered.com/'
+    # base_url = 'https://store.steampowered.com/'
     # special_offer_url = 'https://store.steampowered.com/specials?snr=1_4_4_#tab=TopSellers'
-    special_offers = 'facetedbrowse_FacetedBrowseItems_NO-IP'
+    # special_offers = 'facetedbrowse_FacetedBrowseItems_NO-IP'
     special_offer_url = 'https://store.steampowered.com/specials?flavor=contenthub_all&offset=62'
 
     # def __init__(self) -> None:
